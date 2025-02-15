d269880d33534cb4232cfd825fccb229acb13fc6
Tejas Nikhar <tejastn10@gmail.com>
2025-02-15 18:23:29 +0530

    feat: add favicon, SVG icons, and initial layout with theme provider

---
+++ src/app/layout.tsx
@@ -0,0 +1,61 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+
+import { Outfit } from "next/font/google";
+
+import { ThemeProvider } from "@/components/theme/ThemeProvider";
+
+import { DATA } from "@/data/data";
+
+const outfit = Outfit({
+	subsets: ["latin"],
+	variable: "--font-outfit",
+	display: "swap",
+});
+
+export const metadata: Metadata = {
+	title: {
+		default: DATA.name,
+		template: `%s | ${DATA.name}`,
+	},
+	description: DATA.description,
+	openGraph: {
+		title: `${DATA.name}`,
+		description: DATA.description,
+		siteName: `${DATA.name}`,
+		locale: "en_US",
+		type: "website",
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+	verification: {
+		google: "",
+		yandex: "",
+	},
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en" suppressHydrationWarning className={outfit.variable}>
+			<body className={`${outfit.variable} antialiased`}>
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
+}
