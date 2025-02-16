f3ca82ca4d108218c3236e02c6eeceaacfc91c67
Tejas Nikhar <tejastn10@gmail.com>
2025-02-16 09:02:31 +0530

    feat: replace hover underline with underline slide animation for link interactions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
 			</main>
 			<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+					className="flex items-center gap-2 underline-slide"
 					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
 					target="_blank"
 					rel="noopener noreferrer"
@@ -45,7 +45,7 @@ export default function Home() {
 					Learn
 				</a>
 				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+					className="flex items-center gap-2 underline-slide"
 					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
 					target="_blank"
 					rel="noopener noreferrer"
@@ -54,7 +54,7 @@ export default function Home() {
 					Examples
 				</a>
 				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+					className="flex items-center gap-2 underline-slide"
 					href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
 					target="_blank"
 					rel="noopener noreferrer"
